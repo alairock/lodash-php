99843fa4a4e6ec75e17e466df5c07c8814ef47d7
Skyler Lewis <skyler.lewis@beanstalkhq.com>
2015-06-02 21:05:06 -0600

    adding a variety of tests and methods for arrays.

--- src/alairock/lodash/Arrays.php
+++ src/alairock/lodash/Arrays.php
@@ -8,40 +8,138 @@ use Closure;
 class Arrays
 {
 
-	public $container = [];
+	public $array = [];
 
-	public $propertyTwo = [
-		'asdf', 'asdff'
-	];
+	public $original = [];
 
+	/**
+	 * @param array $array
+	 */
 	public function __construct(Array $array = [])
 	{
-		$this->container = $array;
+		$this->array = $array;
+		$this->original = $array;
 	}
 
+	/**
+	 * @param array $array
+	 * @return $this
+	 */
 	public function from(Array $array)
 	{
-		$this->container = $array;
+		$this->array = $array;
+		$this->original = $array;
 		return $this;
 	}
 
+	/**
+	 * @return array
+	 */
 	public function get()
 	{
-		return $this->container;
+		return $this->array;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function toJson()
 	{
-		return json_encode($this->container);
+		return json_encode($this->array);
 	}
 
+	/**
+	 * each
+	 * @param callable $callback
+	 * @return $this
+	 */
 	public function each(Closure $callback)
 	{
 		$updatedArray = [];
-		foreach ($this->container as $key => $value) {
+		foreach ($this->array as $key => $value) {
 			$updatedArray[$key] = $callback($value, $key);
 		}
-		$this->container = $updatedArray;
+		$this->array = $updatedArray;
+		return $this;
+	}
+
+	/**
+	 * changeKeyCase
+	 * @param int $case
+	 * @return $this
+	 */
+	public function changeKeyCase($case = CASE_LOWER)
+	{
+		$this->array = array_change_key_case($this->array, $case);
+		return $this;
+	}
+
+	/**
+	 * chunk
+	 * @param $size
+	 * @param bool $preserveKeys
+	 * @return $this
+	 */
+	public function chunk($size, $preserveKeys = false)
+	{
+		$this->array = array_chunk($this->array, $size, $preserveKeys);
+		return $this;
+	}
+
+	/**
+	 * column
+	 * @param $columnKey
+	 * @param null $indexKey
+	 * @return $this
+	 */
+	public function column($columnKey, $indexKey = null)
+	{
+		$this->array = array_column($this->array, $columnKey, $indexKey);
+		return $this;
+	}
+
+	/**
+	 * arrayCombine
+	 */
+	public function combine(Array $keys, Array $values)
+	{
+		$this->array = array_combine($keys, $values);
+		return $this;
+	}
+
+	/**
+	 * countValues
+	 */
+	public function countValues()
+	{
+		$this->array = array_count_values($this->array);
+		return $this;
+	}
+
+	/**
+	 * diffAssoc
+	 */
+	public function diffAssoc(...$arrays)
+	{
+		$this->array = call_user_func_array("array_diff_assoc", $arrays);
+		return $this;
+	}
+
+	/**
+	 * diffKey
+	 */
+	public function diffKey(...$arrays)
+	{
+		$this->array = call_user_func_array("array_diff_key", $arrays);
+		return $this;
+	}
+
+	/**
+	 * array_map
+	 */
+	public function array_map(Closure $callback)
+	{
+		array_map($callback, $this->array);
 		return $this;
 	}
 }
--- test/ArrayTest.php
+++ test/ArrayTest.php
@@ -25,6 +25,92 @@ class ArrayTest extends \PHPUnit_Framework_TestCase
 		$this->assertSame([3, 6, 9, 12], $response->get(), 'Something is not going well');
 	}
 
+	/*
+	 * @test
+	 */
+	public function test_array_change_key_case()
+	{
+		$response = $this->array->from(["FirSt" => 1, "SecOnd" => 4])->changeKeyCase(CASE_UPPER);
+		$this->assertSame(["FIRST" => 1, "SECOND" => 4], $response->get(), 'Something is not going well');
+	}
+
+	/*
+	 * @test
+	 */
+	public function test_array_chunk()
+	{
+		$response = $this->array->from(['a', 'b', 'c', 'd', 'e'])->chunk(2);
+		$this->assertSame([['a', 'b'], ['c', 'd'], ['e']], $response->get(), 'Something is not going well');
+	}
+
+	/*
+    * @test
+    */
+	public function test_array_column()
+	{
+		$records = [
+			[
+				'id' => 2135,
+				'first_name' => 'John',
+				'last_name' => 'Doe',
+			],
+			[
+				'id' => 3245,
+				'first_name' => 'Sally',
+				'last_name' => 'Smith',
+			],
+		];
+		$response = $this->array->from($records)->column('first_name');
+		$this->assertSame(['John', 'Sally'], $response->get(), 'Something is not going well');
+	}
+
+	/*
+    * @test
+    */
+	public function test_array_combine()
+	{
+		$response = $this->array->combine(['green', 'red', 'yellow'], ['avocado', 'apple', 'banana']);
+		$this->assertSame(['green' => 'avocado', 'red' => 'apple', 'yellow' => 'banana'], $response->get(), 'Something is not going well');
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_array_count_values()
+	{
+		$response = $this->array->from([1, "hello", 1, "world", "hello"])->countValues();
+		$this->assertSame([1 => 2, "hello" => 2, "world" => 1], $response->get(), 'Something is not going well');
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_array_diff_assoc()
+	{
+		$response = $this->array->diffAssoc(["a" => "green", "b" => "brown", "c" => "blue", "red"], ["a" => "green", "yellow", "red"]);
+		$this->assertSame(['b' => 'brown', 'c' => 'blue', 0 => 'red'], $response->get(), 'Something is not going well');
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_array_diff_key()
+	{
+		$response = $this->array->diffKey(['blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4], ['green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8]);
+		$this->assertSame(['red' => 2, 'purple' => 4], $response->get(), 'Something is not going well');
+	}
+
+
+	/**
+	 * @test
+	 */
+	public function test_array_diff_uassoc()
+	{
+		$response = $this->array->diffKey(['blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4], ['green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8]);
+		$this->assertSame(['red' => 2, 'purple' => 4], $response->get(), 'Something is not going well');
+	}
+
+
 	public function tearDown()
 	{
 		parent::tearDown();
